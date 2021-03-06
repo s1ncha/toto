4a9515fdd165d11933790a3b28956550c96127d2
s1ncha <shintaro.f.1204@gmail.com>
2021-03-06 19:06:48 +0900

    Update rate.pl

--- rate.pl
+++ rate.pl
@@ -6,9 +6,10 @@ use utf8;
 use Chess::Elo qw(:all);
 use Text::CSV::Simple;
 
-@ARGV == 2 or die("Invalid argument.");
+@ARGV == 3 or die("Invalid argument.");
 
-my ($ratingFile, $resultFile) = @ARGV;
+my ($k, $ratingFile, $resultFile) = @ARGV;
+$Chess::Elo::factor = $k;
 
 my $ratingParser = Text::CSV::Simple->new();
 my @ratingData = $ratingParser->read_file($ratingFile);
@@ -17,14 +18,14 @@ my $resultParser = Text::CSV::Simple->new();
 my @resultData = $resultParser->read_file($resultFile);
 
 my %ratingTable;
-foreach my $record (@ratingData){
-	$ratingTable{$record->[0]} = $record->[1];
+foreach my $recode (@ratingData){
+	$ratingTable{$recode->[0]} = $recode->[1];
 }
 
 foreach my $result (@resultData){
-	my ($rating1, $rating2) = elo($ratingTable{$result->[0]}, $result->[4], $ratingTable{$result->[3]});
-	$ratingTable{$result->[0]} = $rating1;
-	$ratingTable{$result->[3]} = $rating2;
+	my ($rating1, $rating2) = elo($ratingTable{$result->[2]}, $result->[6], $ratingTable{$result->[5]});
+	$ratingTable{$result->[2]} = $rating1;
+	$ratingTable{$result->[5]} = $rating2;
 }
 
 foreach my $key (keys(%ratingTable)){
